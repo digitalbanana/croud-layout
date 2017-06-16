3e37d4a5839d3e1a33e6ae5ad9e3c6474b0fb3f0
Brock <brock.reece@croud.co.uk>
2017-06-16 12:42:42 +0100

    Fix typo

--- src/plugin.js
+++ src/plugin.js
@@ -5,7 +5,7 @@ export default {
     install(Vue, options) {
         Vue.component('croud-layout', croudLayout)
 
-        options.store.registerModule('universal', currentUser)
+        options.store.registerModule('universal', universal)
         /* eslint-disable no-underscore-dangle */
         Object.keys(options.store._actions).filter(a => a.includes('$init')).forEach(a => options.store.dispatch(a, a))
     },
