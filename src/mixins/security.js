f33ffcc183b0ce66814e31bc6d1ca297c53c6e16
Brock <brock.reece@croud.co.uk>
2017-07-03 12:29:15 +0100

    Add security mixin

---
+++ src/mixins/security.js
@@ -0,0 +1,61 @@
+import { mapGetters } from 'vuex'
+
+export default {
+    data() {
+        return {
+            permission: {
+                create: false,
+                read: false,
+                update: false,
+                delete: false,
+            },
+            security: {
+                create: false,
+                read: false,
+                update: false,
+                delete: false,
+            },
+            allowOwner: false,
+        }
+    },
+
+    computed: {
+        ...mapGetters({
+            effectivePermissions: 'universal/effectivePermissions',
+            session: 'universal/user',
+        }),
+
+        owner() {
+            return this.user && this.user.code === this.session.code
+        },
+    },
+
+    methods: {
+        setSecurity() {
+            Object.keys(this.permission).forEach((key) => {
+                if (!this.permission[key]) {
+                    return
+                }
+
+                if ((this.allowOwner && this.owner) ||
+                    Object.values(this.effectivePermissions).indexOf(this.permission[key]) !== -1 ||
+                    Object.values(this.effectivePermissions).indexOf(this.permission[key].replace(/\.\w+$/, '.*')) !== -1 ||
+                    Object.values(this.effectivePermissions).indexOf(this.permission[key].replace(/(\.\w+){2}$/, '.*.*')) !== -1) {
+                    this.security[key] = true
+                } else {
+                    this.security[key] = false
+                }
+            })
+        },
+    },
+
+    created() {
+        this.setSecurity()
+    },
+
+    watch: {
+        effectivePermissions() {
+            this.setSecurity()
+        },
+    },
+}
