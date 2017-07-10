6a4958ea74acf6700147665115a1ad4c00653df0
Brock <brock.reece@croud.co.uk>
2017-07-10 15:44:04 +0100

    Add vue-resource $resource polyfill

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,38 @@
 import Vue from 'vue'
 import axios from 'axios'
 import qs from 'qs'
+import urlTemplate from 'url-template'
+
+const actions = {
+    get: { method: 'get' },
+    save: { method: 'post' },
+    query: { method: 'get' },
+    update: { method: 'put' },
+    remove: { method: 'delete' },
+    delete: { method: 'delete' },
+}
+
+
+const buildRequest = (url, action, args) => {
+    const local = action
+    local.url = url.expand(args[0])
+
+    if (args.length > 1) {
+        local.body = args[1]
+    } else {
+        switch (action.method) {
+        case 'post':
+        case 'put':
+            local.body = args
+            break
+
+        default:
+            local.params = args
+            break
+        }
+    }
+    return local
+}
 
 export default {
     install(vue, options = {}) {
@@ -11,6 +43,7 @@ export default {
         }
 
         const instance = axios.create(options.defaults)
+
         instance.interceptors.response.use((response) => {
             response.body = response.data
             return response
@@ -18,5 +51,18 @@ export default {
 
         Vue.prototype.$http = instance
         Vue.http = instance
+
+        const resource = (url) => {
+            const axiosResource = {}
+
+            Object.keys(actions).forEach((alias) => {
+                resource[alias] = (...args) => instance.request(buildRequest(urlTemplate.parse(url), actions[alias], args))
+            })
+
+            return axiosResource
+        }
+
+        Vue.prototype.$resource = resource
+        Vue.resource = resource
     },
 }
